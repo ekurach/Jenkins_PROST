056793c55450d64deceb01109db335806d2fd443
ekurach <emiliakurach@o2.pl>
2020-04-29 03:44:43 +0200

    Dodaj pliki projektów.

---
+++ Jenkins_PROST.cpp
@@ -0,0 +1,12 @@
+﻿// Jenkins_PROST.cpp: definiuje punkt wejścia dla aplikacji.
+//
+
+#include "Jenkins_PROST.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
